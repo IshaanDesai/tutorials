ed317e4bb0d14b20bd168590bec379d72e84b6ce
Ishaan Desai <desaiin@ipvs.uni-stuttgart.de>
2024-04-09 17:10:23 +0200

    Add job submission script

---
+++ two-scale-heat-conduction/submit-job.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Job name:
+#SBATCH -J multi-precice
+#
+# Error and Output files
+#SBATCH -o ./%x.%j.out
+#SBATCH -e ./%x.%j.err
+#
+# Working directory:
+#SBATCH -D ./
+#
+#Notification and type
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ishaan.desai@ipvs.uni-stuttgart.de
+#
+# Wall clock limit:
+#SBATCH --time=01:00:00
+#
+# Compute resources
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=65
+
+echo "SLURM_NNODES"=$SLURM_NNODES
+echo "working directory="$SLURM_SUBMIT_DIR
+
+# load the modules you need
+#module purge
+module load ipvs-epyc/gcc/10.2 ipvs-epyc/openmpi/4.0.4-gcc-10.2 ipvs-epyc/python/3.8.5 ub2004/libxml2/2.9.10 ub2004/boost/1.75.0
+#module list
+
+echo "Launching macro participant"
+cd macro-nutils/
+mpiexec -n 1 --bind-to core python3 macro.py verbose=2 &> log_macro.log &
+
+cd ..
+
+echo "Launching micro manager"
+cd micro-nutils/
+mpiexec -n 64 --bind-to core python3 run_micro_manager.py verbose=2 &> log_micro.log
+
+echo "Simulation completed."
